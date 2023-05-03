732fd518e45b4c4a5961106a794b9f620621fdc8
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-05-03 00:02:41 -0300

    refactor: creating a home

---
+++ app/src/main/java/com/adriana/barbershop/view/Home.kt
@@ -0,0 +1,12 @@
+package com.adriana.barbershop.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adriana.barbershop.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
